6c2f26638a2babddcf52dc952ca11e5bd3269a7b
Allie <alliecaton3@gmail.com>
2021-03-04 15:05:58 -0500

    user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+      has_many :shelves
+      has_many :books, foreign_key: :author_id
+      
 end
