01d79914cee48543fa2db2b65b2033eab6ac0d16
Allie <alliecaton3@gmail.com>
2021-03-04 15:40:42 -0500

    set up nested resource creation of shelves

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -8,20 +8,25 @@ class ShelvesController < ApplicationController
     end
 
     def new
-        @shelf = Shelf.new
+        user = User.find_by(id: params[:user_id])
+        if params[:user_id]
+            redirect_to user_path(user)
+        else
+            @shelf = Shelf.new(user_id: params[:user_id])
+        end
     end 
 
     def create
         @shelf = Shelf.new(shelf_params)
-        # @shelf.user = current_user
+        @shelf.user = current_user
         @shelf.save 
         redirect_to shelf_path(@shelf)
     end
 
     private
 
-    def list_params
-        params.require(:shelf).permit(:name)
+    def shelf_params
+        params.require(:shelf).permit(:name, :user_id)
     end 
 
 end
