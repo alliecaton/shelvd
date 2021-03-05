02f7cf2e8b558dfe35a395a3e713d57d3a906905
Allie <alliecaton3@gmail.com>
2021-03-05 15:17:36 -0500

    hooks set up to create book from show page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,15 +8,18 @@ class BooksController < ApplicationController
     end 
 
     def show 
+        @shelves = current_user.shelves
         isbn = params[:id]
         @display_book = Book.search("isbn:#{isbn}").first
         @book = Book.new(title: @display_book.title, description: @display_book.description, average_rating: @display_book.average_rating)
     end 
 
-    # def new 
-    # end
+    def new 
+        @book = Book.new
+    end
 
     def create 
+        byebug
         @book = Book.new(book_params)
         @book.save
         redirect_to book_path(@book)
