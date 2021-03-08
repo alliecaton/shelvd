45b10ab99c58489f587e9db58f16654d8c169fe0
Allie <alliecaton3@gmail.com>
2021-03-08 14:30:46 -0500

    omniauth setup not complete

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'googlebooks'
 gem 'bootsnap', '>= 1.4.4', require: false
 
 gem 'devise'
+gem 'dotenv'
+gem 'omniauth-github', '1.4.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
 
       has_many :shelves
       has_many :books, through: :shelves
+
+      def self.from_omniauth(auth)
+        where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+          user.provider = auth.provider
+          # user.name = auth.info.name
+          user.uid = auth.uid
+          user.email = auth.info.email
+          user.password = Devise.friendly_toke[0.20]
+        end
+      end
 
 end
