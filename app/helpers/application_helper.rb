83d9a2d4b993db22d5c70049271c5d9137e93247
Allie <alliecaton3@gmail.com>
2021-03-08 12:21:29 -0500

    display authors on book page fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,14 +7,6 @@ module ApplicationHelper
     def unslug(title)
         title.split("-").join(" ")
     end
-
-    private 
-
-    def create_author(book)
-        params[:book][:authors].map do |author_name|
-            Author.find_or_create(name: author_name)
-        end
-    end 
     
 
 end
--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -1,2 +1,14 @@
 module AuthorsHelper
+
+    def authors_display
+        if @display_book.authors.length > 0
+            @display_book.authors.map do |author|
+                author.name
+            end.join(", ")
+        else 
+            @authors
+        end 
+    end
+
+
 end
