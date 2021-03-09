7e3baefb2083f9ae73a7f5d9abc0f923ddd6d887
Allie <alliecaton3@gmail.com>
2021-03-09 14:08:01 -0500

    fixed search bar not rendering when searching from pages other than index

--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -3,7 +3,7 @@ module AuthorsHelper
 
     def find_create(book)
         book.authors.split(",").map do |author_name|
-            Author.find_or_create_by(name: author_name).name
+            author= Author.find_or_create_by(name: author_name)
         end.join(", ")
     end
     
