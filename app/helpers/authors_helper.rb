e4ef26f891acfc34ec4642c6dcd481709b2d8c8a
Allie <alliecaton3@gmail.com>
2021-03-09 15:03:45 -0500

    bug fix with displaying author name on book show pages

--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -2,9 +2,15 @@ module AuthorsHelper
 
 
     def find_create(book)
-        book.authors.split(",").map do |author_name|
-            author= Author.find_or_create_by(name: author_name)
-        end.join(", ")
+        book.authors.split(", ").map do |author_name|
+            Author.find_or_create_by(name: author_name)
+        end
+    end
+
+    def find_author(authors_array)
+        authors_array.map do |author|
+            Author.find_by(name: author.name)
+        end 
     end
     
 
