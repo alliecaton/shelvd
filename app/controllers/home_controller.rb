b90f610d6bbece2a80385ccdeb2c88fb73dee900
Allie <alliecaton3@gmail.com>
2021-03-12 14:19:45 -0500

    big with duplicate books showing up on the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     # @books = Book.highest_rated
-    @books = Book.highest_rated_sorted[0..7]
+    @books = Book.highest_rated_sorted[0..7].uniq
   end
 end
